3d36cc212991473908c232e31c1b96eaf93b8304
adil <adilaitelhoucine1@gmail.com>
2025-03-13 10:35:24 +0100

    Feat : Authentification Avec Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,7 +12,14 @@ Route::get('/user', function (Request $request) {
 Route::get('/hello', function (Request $request) {
     return response()->json(['message'=>'tbnksnis ']);
 });
-Route::apiResource("quotes", QuoteController::class);
-Route::get('/quotes/random/{count}', [QuoteController::class, 'random']);
-Route::get('/quotes/GetQuoteWithLength/{length}', [QuoteController::class, 'GetQuoteWithLength']);
-Route::get('/Popular', [QuoteController::class, 'GetPopularQuote']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource("quotes", QuoteController::class);
+    Route::get('/quotes/random/{count}', [QuoteController::class, 'random']);
+    Route::get('/quotes/GetQuoteWithLength/{length}', [QuoteController::class, 'GetQuoteWithLength']);
+    Route::get('/Popular', [QuoteController::class, 'GetPopularQuote']);
+});
